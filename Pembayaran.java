aa1cd059028ba391df8a3fd52d528a5cc6b16334
azzariyatazra <133877452+azzariyatazra@users.noreply.github.com>
2023-11-28 22:29:13 +0700

    Update Pembayaran.java

--- Pembayaran.java
+++ Pembayaran.java
@@ -1,5 +1,10 @@
 public abstract class Pembayaran {
     protected String id;
+    protected Transaksi transaksi; // tambahkan atribut transaksi
+
+    public Pembayaran(Transaksi transaksi) {
+        this.transaksi = transaksi;
+    }
 
     public abstract void metodePembayaran();
 
