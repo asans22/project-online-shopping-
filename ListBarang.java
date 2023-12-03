7843e70563e459eb3900d81f478680dc0642b0be
asans22 <ahmadsyahrmdn@gmail.com>
2023-12-03 23:39:54 +0700

    add javadoc in ListBarang.java

--- ListBarang.java
+++ ListBarang.java
@@ -8,22 +8,25 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+// Class untuk mengelola daftar barang dan melakukan operasi seperti menambah, mengedit, dan menghapus barang.
 public class ListBarang {
-    // class untuk melihat daftar barang1 yang ada
-    private ArrayList<Barang> barang;
+    private ArrayList<Barang> barang;      // Daftar barang di inventaris
+    private ArrayList<Barang> keranjang;   // Daftar barang di keranjang belanja
 
-    public ListBarang(){
+    // Konstruktor untuk menginisialisasi daftar barang.
+    public ListBarang() {
         this.barang = new ArrayList<Barang>();
+        this.keranjang = new ArrayList<Barang>();
     }
 
-    //method baca database
-    private void bacaDatabase(){
+    // Metode untuk membaca data barang dari file database.
+    private void bacaDatabase() {
         BufferedReader databaseBarang = null;
         String bacaDatabeseBarang;
         String pathDatabase = "List Barang.txt";
-        try{
+        try {
             databaseBarang = new BufferedReader(new FileReader(pathDatabase));
-            while((bacaDatabeseBarang=databaseBarang.readLine())!= null){
+            while ((bacaDatabeseBarang = databaseBarang.readLine()) != null) {
                 Barang barang1 = new Barang();
                 String[] token = bacaDatabeseBarang.split(" ");
 
@@ -33,50 +36,50 @@ public class ListBarang {
                 barang1.setHarga(Integer.parseInt(token[3]));
 
                 this.barang.add(barang1);
-
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println(e);
-        }finally{
-            try{
-                if(databaseBarang != null){
+        } finally {
+            try {
+                if (databaseBarang != null) {
                     databaseBarang.close();
                 }
-            }catch(IOException e){
+            } catch (IOException e) {
                 System.out.println(e);
             }
         }
     }
-    public void tampilkanListBarang(){
+
+    // Metode untuk menampilkan daftar barang.
+    public void tampilkanListBarang() {
         this.bacaDatabase();
 
-        barang.forEach((barang1)->{
-            System.out.println("kode barang : " +barang1.getKodeBarang());
-            System.out.println("nama barang : " +barang1.getNamaBarang());
-            System.out.println("stok barang : " +barang1.getStok());
-            System.out.println("harga barang : " +barang1.getHarga());
+        barang.forEach((barang1) -> {
+            System.out.println("kode barang : " + barang1.getKodeBarang());
+            System.out.println("nama barang : " + barang1.getNamaBarang());
+            System.out.println("stok barang : " + barang1.getStok());
+            System.out.println("harga barang : " + barang1.getHarga());
             System.out.println(" ");
         });
 
-        
         this.barang.clear();
-
     }
 
-    public void tambahBarang(){
+    // Metode untuk menambah barang ke inventaris.
+    public void tambahBarang() {
         this.bacaDatabase();
 
         Scanner s = new Scanner(System.in);
 
         System.out.print("masukkan kode barang : ");
         String kodeBarang = s.nextLine();
-        
+
         System.out.print("masukkan nama barang : ");
         String namaBarang = s.nextLine();
-        
+
         System.out.print("masukkan  stok : ");
         int stok = s.nextInt();
-        
+
         System.out.print("masukkan harga : ");
         int harga = s.nextInt();
 
@@ -89,52 +92,54 @@ public class ListBarang {
 
         this.barang.add(newBarang);
         this.writeDatabase();
-
-
+        this.barang.clear();
     }
-    private void writeDatabase(){
+
+    // Metode untuk menulis data barang ke file database.
+    private void writeDatabase() {
         BufferedWriter databaseBarang = null;
-        // String tulisDatabaseBarang;
         String pathDatabase = "List Barang.txt";
 
-        try{
+        try {
             databaseBarang = new BufferedWriter(new FileWriter(pathDatabase));
-            for(Barang barang1 : this.barang){
+            for (Barang barang1 : this.barang) {
                 String line = String.format("%s %s %d %d",
-                barang1.getKodeBarang(),
-                barang1.getNamaBarang(),
-                barang1.getStok(),
-                barang1.getHarga());
+                        barang1.getKodeBarang(),
+                        barang1.getNamaBarang(),
+                        barang1.getStok(),
+                        barang1.getHarga());
 
                 databaseBarang.write(line);
                 databaseBarang.newLine();
-           }
-        } catch(IOException e){
-                System.out.println(e);
-        }finally{
-            try{
-                if(databaseBarang != null){
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            try {
+                if (databaseBarang != null) {
                     databaseBarang.close();
                 }
-            }catch(IOException e){
+            } catch (IOException e) {
                 System.out.println(e);
             }
         }
     }
-    public void editBarang(){
+
+    // Metode untuk mengedit data barang di inventaris.
+    public void editBarang() {
         this.bacaDatabase();
         BufferedReader bacaDatabase = null;
         Scanner s = new Scanner(System.in);
         Barang barangEdit = new Barang();
         String kodeBarang;
-        System.out.println("===  EDIT BARANG ===");
+        System.out.println("==== EDIT BARANG ====");
         System.out.print("masukkan kode barang : ");
         kodeBarang = s.nextLine();
 
         try {
-            
-            for(int i=0 ; i<this.barang.size();i++){
-                if(kodeBarang.equals(this.barang.get(i).getKodeBarang())){
+
+            for (int i = 0; i < this.barang.size(); i++) {
+                if (kodeBarang.equals(this.barang.get(i).getKodeBarang())) {
                     System.out.println("edit barang " + this.barang.get(i).getNamaBarang());
                     System.out.print("masukkan stok : ");
                     String newStok = s.nextLine();
@@ -153,42 +158,97 @@ public class ListBarang {
             this.writeDatabase();
             this.barang.clear();
         } catch (Exception e) {
-            // TODO: handle exception
             System.out.println(e);
-        }finally{
-            if(bacaDatabase != null){
+        } finally {
+            if (bacaDatabase != null) {
                 try {
                     bacaDatabase.close();
                 } catch (IOException e) {
-                    
                     System.out.println((e));
                 }
             }
-        
         }
-
-        
-         
     }
-    public void hapusBarang(){
+
+    // Metode untuk menghapus barang dari inventaris.
+    public void hapusBarang() {
         this.bacaDatabase();
         Scanner s = new Scanner(System.in);
         String kodeBarang;
-        System.out.println("===  HAPUS BARANG ===");
+        System.out.println("==== HAPUS BARANG ====");
         System.out.print("masukkan kode barang : ");
         kodeBarang = s.nextLine();
 
-        for(int i = 0 ; i<this.barang.size();i++){
-            if(kodeBarang.equals(this.barang.get(i).getKodeBarang())){
+        for (int i = 0; i < this.barang.size(); i++) {
+            if (kodeBarang.equals(this.barang.get(i).getKodeBarang())) {
                 this.barang.remove(i);
             }
         }
         this.writeDatabase();
         this.barang.clear();
-
     }
-    
 
-    
+    // Metode untuk membaca data barang dari file database keranjang.
+    public void bacaDatabaseKeranjang() {
+        BufferedReader databaseKeranjang = null;
+        String bacaDatabaseKeranjang;
+        String pathReaderKeranjang = "Keranjang.txt";
 
+        try {
+            databaseKeranjang = new BufferedReader(new FileReader(pathReaderKeranjang));
+            while ((bacaDatabaseKeranjang = databaseKeranjang.readLine()) != null) {
+                Barang keranjang1 = new Barang();
+                String[] token = bacaDatabaseKeranjang.split(" ");
+
+                keranjang1.setKodeBarang(token[0]);
+                keranjang1.setNamaBarang(token[1]);
+                keranjang1.setStok(Integer.parseInt(token[2]));
+                keranjang1.setHarga(Integer.parseInt(token[3]));
+
+                this.keranjang.add(keranjang1);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            try {
+                if (databaseKeranjang != null) {
+                    databaseKeranjang.close();
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    // Metode untuk melakukan transaksi barang.
+    public void transaksiBarang() {
+        this.bacaDatabase();
+        this.bacaDatabaseKeranjang();
+
+        for (Barang barangTransaksi : this.barang) {
+            for (Barang keranjangItem : this.keranjang) {
+                if (barangTransaksi.getKodeBarang().equals(keranjangItem.getKodeBarang())) {
+                    Barang barangTransaksiBaru = new Barang();
+                    barangTransaksiBaru.setKodeBarang(barangTransaksi.getKodeBarang());
+                    barangTransaksiBaru.setNamaBarang(barangTransaksi.getNamaBarang());
+
+                    // Hitung sisa stok setelah transaksi
+                    int sisaStok = barangTransaksi.getStok() - keranjangItem.getStok();
+
+                    // Pastikan sisa stok tidak negatif
+                    if (sisaStok >= 0) {
+                        barangTransaksiBaru.setStok(sisaStok);
+                        barangTransaksiBaru.setHarga(barangTransaksi.getHarga());
+
+                        this.barang.set(this.barang.indexOf(barangTransaksi), barangTransaksiBaru);
+                    } else {
+                        System.out.println("Stok tidak cukup untuk produk dengan kode " + barangTransaksiBaru.getKodeBarang());
+                    }
+                }
+            }
+        }
+
+        // Perbarui database inventaris
+        this.writeDatabase();
+    }
 }
