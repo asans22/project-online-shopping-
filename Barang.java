7a503237bc9488a55d9693a518221a58e91e382d
asans22 <ahmadsyahrmdn@gmail.com>
2023-12-03 23:37:01 +0700

    add javadoc in Barang.java

--- Barang.java
+++ Barang.java
@@ -1,41 +1,108 @@
-
+/**
+ * Kelas {@code Barang} merepresentasikan suatu barang dengan informasi seperti kode barang, nama barang, stok, dan harga.
+ * Kelas ini memiliki metode-metode untuk mendapatkan dan mengatur informasi barang.
+ */
 public class Barang {
-    // class untuk menambahkan,mengedit dan menghapus barang
     private String kodeBarang;
     private String namaBarang;
     private int stok;
     private int harga;
 
-    public Barang(){
+    /**
+     * Konstruktor default untuk membuat instance {@code Barang} dengan nilai default.
+     * Nilai default diatur sebagai kodeBarang "000", namaBarang "not found", stok 0, dan harga 0.
+     */
+    public Barang() {
         kodeBarang = "000";
         namaBarang = "not found";
         stok = 0;
         harga = 0;
     }
-    public String getKodeBarang(){
+
+    /**
+     * Metode untuk mendapatkan kode barang.
+     *
+     * @return Kode barang.
+     */
+    public String getKodeBarang() {
         return this.kodeBarang;
     }
-    public void setKodeBarang(String kodeBarang){
+
+    /**
+     * Metode untuk mengatur kode barang.
+     *
+     * @param kodeBarang Kode barang baru yang akan diatur.
+     */
+    public void setKodeBarang(String kodeBarang) {
         this.kodeBarang = kodeBarang;
     }
-    public String getNamaBarang(){
+
+    /**
+     * Metode untuk mendapatkan nama barang.
+     *
+     * @return Nama barang.
+     */
+    public String getNamaBarang() {
         return this.namaBarang;
     }
-    public void setNamaBarang(String namaBarang){
+
+    /**
+     * Metode untuk mengatur nama barang.
+     *
+     * @param namaBarang Nama barang baru yang akan diatur.
+     */
+    public void setNamaBarang(String namaBarang) {
         this.namaBarang = namaBarang;
     }
-    public int getStok(){
+
+    /**
+     * Metode untuk mendapatkan stok barang.
+     *
+     * @return Jumlah stok barang.
+     */
+    public int getStok() {
         return this.stok;
     }
-    public void setStok(int stok){
+
+    /**
+     * Metode untuk mengatur stok barang.
+     *
+     * @param stok Jumlah stok barang baru yang akan diatur.
+     */
+    public void setStok(int stok) {
         this.stok = stok;
     }
-    public int getHarga(){
+
+    /**
+     * Metode untuk mendapatkan harga barang.
+     *
+     * @return Harga barang.
+     */
+    public int getHarga() {
         return this.harga;
     }
-    public void setHarga(int harga){
+
+    /**
+     * Metode untuk mengatur harga barang.
+     *
+     * @param harga Harga barang baru yang akan diatur.
+     */
+    public void setHarga(int harga) {
         this.harga = harga;
     }
 
-    
+    /**
+     * Metode untuk menghasilkan representasi string dari objek {@code Barang}.
+     *
+     * @return String representasi dari objek {@code Barang}.
+     */
+    @Override
+    public String toString() {
+        return "Barang{" +
+                "kodeBarang='" + kodeBarang + '\'' +
+                ", namaBarang='" + namaBarang + '\'' +
+                ", stok=" + stok +
+                ", harga=" + harga +
+                '}';
+    }
 }
